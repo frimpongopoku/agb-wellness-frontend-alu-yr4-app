e4bfb56d06d1206928bd6d86130cb10390f217d6
frimpongopoku <pongofrimi@gmail.com>
2021-12-30 21:02:03 +0000

    Added react router and some commonly used css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import Router from "./Router";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
