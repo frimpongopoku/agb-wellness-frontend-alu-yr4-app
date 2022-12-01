6e8eef3bd587968628cbeb5d3c1cc32511f07749
frimpongOpoku <pongofrimi@gmail.com>
2022-12-01 11:57:33 +0400

    regsitration page all setup except btn

--- src/pages/auth/Authentication.js
+++ src/pages/auth/Authentication.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "../../components/texfield/TextField";
 import Login from "./Login";
+import Registration from "./Registration";
 
 function Authentication() {
+  const [currentTab, setCurrentTab] = useState("registration");
+  const TABS = {
+    login: { key: "login", component: <Login /> },
+    registration: { key: "registration", component: <Registration /> },
+  };
+
+  const component = TABS[currentTab].component;
   return (
     <div className="auth-root">
       <div className="auth-tab-selector">
-        <h5 className="auth-tab-selected">LOGIN</h5>
-        <h5>REGISTRATION</h5>
+        <h5
+          className={`${
+            currentTab === TABS.login.key ? "auth-tab-selected" : ""
+          }`}
+          onClick={() => setCurrentTab(TABS.login.key)}
+        >
+          LOGIN
+        </h5>
+        <h5
+          className={`${
+            currentTab === TABS.registration.key ? "auth-tab-selected" : ""
+          }`}
+          onClick={() => setCurrentTab(TABS.registration.key)}
+        >
+          REGISTRATION
+        </h5>
       </div>
-      <div className="auth-content-div">
-        <Login />
-      </div>
+      <div className="auth-content-div">{component}</div>
     </div>
   );
 }
---
+++ src/pages/auth/Registration.js
@@ -0,0 +1,36 @@
+import React from "react";
+import TextField from "../../components/texfield/TextField";
+
+function Registration() {
+  return (
+    <div>
+      <TextField
+        label="Email addresss"
+        type="email"
+        placeholder="Enter email address..."
+      />
+      <TextField
+        label="Your name"
+        type="text"
+        placeholder="What would you like to be called?"
+      />
+      <TextField
+        label="Date of birth"
+        type="date"
+        placeholder="Enter your date of birth..."
+      />
+      <TextField
+        label="Your Password"
+        type="password"
+        placeholder="Enter your password..."
+      />
+      <TextField
+        label="Confirm Password"
+        type="password"
+        placeholder="Confirm your password..."
+      />
+    </div>
+  );
+}
+
+export default Registration;
