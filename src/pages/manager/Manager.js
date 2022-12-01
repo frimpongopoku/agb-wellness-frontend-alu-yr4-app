011d9bb175e25befd18b288144a86449b937f988
frimpongOpoku <pongofrimi@gmail.com>
2022-12-02 02:38:51 +0400

    manager demacation setup with dummy values as well

--- src/pages/manager/Manager.js
+++ src/pages/manager/Manager.js
@@ -6,16 +6,23 @@ import PageWrapper from "../../components/PageWrapper";
 function Manager() {
   return (
     <PageWrapper cornerContent={<h3>H3 tag</h3>}>
-      <PageTitle
-        title="Manager"
-        subtitle="As a manager, you can add staff and create categories here"
-      />
-      <div>
-        <div style={{ display: "flex", flexDirection: "row", marginTop: 15 }}>
-          <Button className="add-staff-btn elevate-2">ADD STAFF </Button>
-          <Button className="add-cat-btn elevate-2">ADD CATEGORY </Button>
+      <>
+        <PageTitle
+          title="Manager"
+          subtitle="As a manager, you can add staff and create categories here"
+        />
+        <div>
+          <div style={{ display: "flex", flexDirection: "row", marginTop: 15 }}>
+            <Button className="add-staff-btn elevate-2">ADD STAFF </Button>
+            <Button className="add-cat-btn elevate-2">ADD CATEGORY </Button>
+          </div>
+
+          <div className="manager-partition">
+            <div className="staff-area"></div>
+            <div className="categories-area"></div>
+          </div>
         </div>
-      </div>
+      </>
     </PageWrapper>
   );
 }
