bfb946a7796dfb629585815338b0b67c469397f6
frimpongOpoku <pongofrimi@gmail.com>
2022-12-02 14:34:51 +0400

    userinfo dialog added

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -1,12 +1,15 @@
 import React from "react";
 import CornerTriangle from "./CornerTriangle";
 import PageSkeleton from "./PageSkeleton";
+import UserInfo from "./UserInfo";
 
 function PageWrapper({ children, cornerContent }) {
   return (
     <PageSkeleton>
       <>
-        <CornerTriangle>{cornerContent}</CornerTriangle>
+        <CornerTriangle>
+          <UserInfo />
+        </CornerTriangle>
         <div className="app-page">
           <div className="page-content">{children}</div>
         </div>
---
+++ src/components/UserInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UserInfo() {
+  return (
+    <div className="user-info">
+      <img src="https://i.pravatar.cc/300" />
+      <h3>Rosalinda</h3>
+      <p>@Manager</p>
+    </div>
+  );
+}
+
+export default UserInfo;
